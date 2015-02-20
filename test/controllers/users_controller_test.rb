34a4aab6b57ee54c2f939c5d84930844612268ff
Ante Wall <antewall@gmail.com>
2015-02-20 15:53:11 +0100

    Removed tests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,19 +1,6 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-  test "should get show" do
-    get :show
-    assert_response :success
-  end
 
-  test "should get update" do
-    get :update
-    assert_response :success
-  end
-
-  test "should get me" do
-    get :me
-    assert_response :success
-  end
 
 end
