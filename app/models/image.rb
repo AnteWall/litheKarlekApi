bd0fc4e6af0c8d32c7b795c1442fc397f45cc48b
Ante Wall <antewall@gmail.com>
2015-02-22 18:42:17 +0000

    Fixed Production

--- app/models/image.rb
+++ app/models/image.rb
@@ -7,7 +7,7 @@ class Image < ActiveRecord::Base
     #base = "http://192.168.1.102:3000"
     base = "http://localhost:3000"
     if Rails.env.production?
-      base = "http://lithekarlek-dev.herokuapp.com"
+      base = "http://lithekarlek.klante.webfactional.com"
     end
     base + self.file.url
   end
